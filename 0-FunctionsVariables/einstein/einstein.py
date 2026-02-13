fab02e61f101c465de3bb6ac0f1f14a0f70dc7d8
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-02-13 00:47:01 +0000

    Upload completed einstein.py

---
+++ 0-FunctionsVariables/einstein/einstein.py
@@ -0,0 +1,24 @@
+kg = int(input("enter a number to convert from kg to Joules: "))
+
+total = 0
+
+print(
+    int(total + kg)
+    * 9
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+    * 10
+)
