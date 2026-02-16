044e133df55b5afe82d487a8ad0cf1e6076eabae
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-02-16 22:17:48 +0000

    Finish lecture 1 Conditionals

--- parity.py
+++ parity.py
@@ -1,1 +1,13 @@
-x = int(input("What's x? "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+
+def is_even(n):
+    return n % 2 == 0
+
+
+main()
