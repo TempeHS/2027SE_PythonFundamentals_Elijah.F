6f7acdf03661f7af12a70cbcb20bbb121a2710a9
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-02-25 06:40:42 +0000

    Upload completed plates.py

--- 2-Loops/plates/plates.py
+++ 2-Loops/plates/plates.py
@@ -1,17 +1,58 @@
 def main():
     plate = input("Enter plate: ")
-    if is_valid(plate):
+    if (
+        character_range(plate)
+        and first_two_letters(plate)
+        and no_punctuation(plate)
+        and numbers_at_end(plate)
+    ):
         print("Valid")
     else:
         print("Invalid")
 
 
-def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
+def character_range(plate):
+    if len(plate) >= 2 and len(plate) <= 6:
+        print("Character requirement met")
+        return True
+    else:
+        print("Character requirement has not been met")
         return False
 
-    if not s[0].isalpha() and s[1].isalpha():
+
+def first_two_letters(plate):
+    if plate[0].isalpha() and plate[1].isalpha():
+        print("First two characters are letters")
+        return True
+    else:
+        print("First two characters must be letters, try again")
         return False
+
+
+def no_punctuation(plate):
+    if plate.isalnum():
+        print("No punctuation requirement met")
+        return True
+    else:
+        print("No punctuation is allowed")
+        return False
+
+
+def numbers_at_end(plate):
+    number_started = False
+    for character in plate:
+        if character.isdigit():
+            if not number_started:
+                if character == "0":
+                    print("0 cannot be inputted first, retry")
+                    return False
+                number_started = True
+        else:
+            if number_started:
+                print("Once numbers can begin, only numbers can follow")
+                return False
+    print("Requirement met, numbers follow numbers")
+    return True
 
 
 main()
