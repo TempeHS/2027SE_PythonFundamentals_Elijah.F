8991e47893f9aa43c16d0d05d2c7ed62c12bac4e
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-02-25 10:30:59 +0000

    Upload completed nutrition.py

--- 2-Loops/nutrition/nutrition.py
+++ 2-Loops/nutrition/nutrition.py
@@ -1,28 +1,28 @@
 fruit = [
-    {"name": "Apple", "calories": "130"}
-    {"name": "Avocado", "calories": "50"}
-    {"name": "Banana", "calories": "110"}
-    {"name": "Cantaloupe", "calories": "50"}
-    {"name": "Grapefruit", "calories": "60"}
-    {"name": "Grapes", "calories": "90"}
-    {"name": "Honeydew Melon", "calories": "50"}
-    {"name": "Kiwifruit", "calories": "90"}
-    {"name": "Lemon", "calories": "15"}
-    {"name": "Lime", "calories": "20"}
-    {"name": "Nectarine", "calories": "60"}
-    {"name": "Orange", "calories": "80"}
-    {"name": "Peach", "calories": "60"}
-    {"name": "Pear", "calories": "100"}
-    {"name": "Pineapple", "calories": "50"}
-    {"name": "Plums", "calories": "70"}
-    {"name": "Strawberries", "calories": "50"}
-    {"name": "Sweet Cherries", "calories": "100"}
-    {"name": "Tangerine", "calories": "50"}
-    {"name": "Watermelon", "calories": "80"}
-        ]
+    {"name": "Apple", "calories": "130"},
+    {"name": "Avocado", "calories": "50"},
+    {"name": "Banana", "calories": "110"},
+    {"name": "Cantaloupe", "calories": "50"},
+    {"name": "Grapefruit", "calories": "60"},
+    {"name": "Grapes", "calories": "90"},
+    {"name": "Honeydew Melon", "calories": "50"},
+    {"name": "Kiwifruit", "calories": "90"},
+    {"name": "Lemon", "calories": "15"},
+    {"name": "Lime", "calories": "20"},
+    {"name": "Nectarine", "calories": "60"},
+    {"name": "Orange", "calories": "80"},
+    {"name": "Peach", "calories": "60"},
+    {"name": "Pear", "calories": "100"},
+    {"name": "Pineapple", "calories": "50"},
+    {"name": "Plums", "calories": "70"},
+    {"name": "Strawberries", "calories": "50"},
+    {"name": "Sweet Cherries", "calories": "100"},
+    {"name": "Tangerine", "calories": "50"},
+    {"name": "Watermelon", "calories": "80"},
+]
 
 user_input = input("Name a fruit: ").capitalize()
-for item in fruit: 
-    if user_input == item["name"]: 
-        print("Calories:", item["calories"]) 
+for item in fruit:
+    if user_input == item["name"]:
+        print("Calories:", item["calories"])
         break
