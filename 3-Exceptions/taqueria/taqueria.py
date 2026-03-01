23cb6b4d181e8a61b070a897119a457eb8960c72
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-01 23:35:43 +0000

    Upload completed taqueria.py

--- 3-Exceptions/taqueria/taqueria.py
+++ 3-Exceptions/taqueria/taqueria.py
@@ -1,16 +1,23 @@
 menu = {
-        "Baja Taco": 4.25,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00,
-    }
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00,
+}
 
+total = 0
 while True:
     try:
-        item = input("Place your order of items: \n")
+        item = input("Item: ").title()
+        total += menu[item]
     except EOFError:
+        print(f"Price: ${total:.2f}")
+        break
+    except KeyError:
+        print("Invalid item, retry: ")
+        break
