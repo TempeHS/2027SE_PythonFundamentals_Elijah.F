9dfdcb934f0c8c1b694c050525e94f3f25bece23
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-03 03:14:50 +0000

    Working on outdated.py

---
+++ 3-Exceptions/outdated/outdated.py
@@ -0,0 +1,25 @@
+def main():
+months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
+user_input = convert(input("Enter a date: ")).split(" ")
+month = user_input.index({months})
+day =
+
+def convert():
+    if user_input
+
+
+main()
