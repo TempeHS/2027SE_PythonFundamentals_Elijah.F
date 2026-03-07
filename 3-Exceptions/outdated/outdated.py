011f71f6aca67ecac5442fc280007be80b2b0280
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-07 04:06:25 +0000

    Upload completed outdated.py

--- 3-Exceptions/outdated/outdated.py
+++ 3-Exceptions/outdated/outdated.py
@@ -21,7 +21,7 @@ while True:
             month = int(parts[0])
             day = int(parts[1])
             year = int(parts[2])
-            if day <= 31 and month <= 12:
+            if 1 <= day <= 31 and 1 <= month <= 12:
                 print(f"{year}-{month:02}-{day:02}")
                 break
 
@@ -33,7 +33,7 @@ while True:
             month_2_int = months.index(month_2) + 1
             day_2 = int(parts_2[1])
             year_2 = int(parts_2[2])
-            if day_2 <= 31 and month_2_int <= 12:
+            if 1 <= day_2 <= 31 and 1 <= month_2_int <= 12:
                 print(f"{year_2}-{month_2_int:02}-{day_2:02}")
                 break
         else:
