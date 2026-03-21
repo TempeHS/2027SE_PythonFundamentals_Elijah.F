80c0ef2a47529ab25830644e4d876f3c6832e9a1
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-21 06:59:29 +0000

    Working on 4.5

--- 4-Libraries/professor/professor.py
+++ 4-Libraries/professor/professor.py
@@ -2,22 +2,20 @@ import random
 
 
 def main():
+    score = 0
     level = get_level()
 
-    if level == 1:
-        random_int = random.randint(0, 9)
-    elif level == 2:
-        random_int = random.randint(10, 99)
-    elif level == 3:
-        random_int = random.randint(100, 999)
-
     for _ in range(10):
-        while True:
+        x = generate_number(level)
+        y = generate_number(level)
+        attempts = 0
+
+        while attempts < 3:
             try:
-                equation = input(f"{random_int} + {random_int}")
-                if equation != random_int + random_int:
+                equation = int(input(f"{x} + {y}"))
+                if equation != x + y:
                     print("EEE")
-                    raise ValueError
+                    attempts = attempts + 1
                 else:
                     score = score + 1
                     break
@@ -25,16 +23,26 @@ def main():
                 continue
 
 
+def generate_number(level):
+    if level == 1:
+        random_int = random.randint(0, 9)
+    elif level == 2:
+        random_int = random.randint(10, 99)
+    elif level == 3:
+        random_int = random.randint(100, 999)
+    return random_int
+
+
 def get_level():
     while True:
         try:
             level = int(input("Enter a level between 1, 2 or 3: "))
-            if level != 1 or level != 2 or level != 3:
+            if level not in [1, 2, 3]:
                 raise ValueError
             else:
-                break
+                return level
         except ValueError:
-            continue
+            pass
 
 
 if __name__ == "__main__":
