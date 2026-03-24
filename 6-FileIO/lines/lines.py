cabc10a71d7c9de006ec7f45ce347fa40fab6f30
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-24 04:47:41 +0000

    Upload completed lines.py

---
+++ 6-FileIO/lines/lines.py
@@ -0,0 +1,26 @@
+import sys
+
+count = 0
+try:
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1].endswith(".py") is False:
+        sys.exit("Not a python file")
+except IndexError:
+    sys.exit("Too few command-line arguments")
+
+try:
+    with open(sys.argv[1], "r") as file:
+        for line in file:
+            if line.lstrip(" ").startswith("#") is True:
+                continue
+            if line.strip() == "":
+                continue
+            else:
+                count += 1
+except FileNotFoundError:
+    sys.exit("File not found")
+
+print(f"Number of lines: {count}")
