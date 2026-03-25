d9d051d16b9a6c9f93dbb5c11e022e0932077709
elijahfallah <elie.fallah@education.nsw.gov.au>
2026-03-25 06:55:06 +0000

    Upload completed pizza.py

---
+++ 6-FileIO/pizza/pizza.py
@@ -0,0 +1,24 @@
+import sys
+from tabulate import tabulate
+import csv
+
+try:
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1].endswith(".csv") is False:
+        sys.exit("Not a CSV file")
+except IndexError:
+    sys.exit("Too few command-line arguments")
+
+try:
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        rows = []
+        for row in reader:
+            rows.append(row.values())
+            pass
+        print(tabulate(rows, headers=reader.fieldnames, tablefmt="grid"))
+except FileNotFoundError:
+    sys.exit("File not found")
